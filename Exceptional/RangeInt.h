34dbfb2e9ef882bed2e9638dbb57ab213df27c55
Fennekin <colin.rexing@gmail.com>
2018-04-17 15:40:49 -0700

    added getters and setters to header file

--- Exceptional/RangeInt.h
+++ Exceptional/RangeInt.h
@@ -4,11 +4,21 @@ class RangeInt
 private:
 	int kUnboundLower_;
 	int kUnboundUpper_;
+	int value_;
 
 	constexpr auto kUnboundLower();
 	constexpr auto kUnboundUpper();
 
 public:
 	RangeInt(int const lower, int const upper);
+	RangeInt();
+	
+	auto get_lower() const;
+	auto get_upper() const;
+	auto get_value()const;
+
+	void set_lower(int num);
+	void set_upper(int num);
+	void set_value(int num);
 };
 
