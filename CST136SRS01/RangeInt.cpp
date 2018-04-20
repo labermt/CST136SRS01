139294b0409be383b62bfae2dc482768b280f9fe
Beau Bell <beauski00@live.com>
2018-04-20 10:24:23 -0700

    Framework set up

--- CST136SRS01/RangeInt.cpp
+++ CST136SRS01/RangeInt.cpp
@@ -1,29 +1,39 @@
 #include "stdafx.h"
 #include "RangeInt.h"
 
-RangeInt::RangeInt(int d0, int d1) : lower_(d0), value_(d0 + 1), upper_(d1)
+RangeInt::RangeInt(const int d0, const int d1) : lower_(d0), value_(d0), upper_(d1), limits_(true)
 {
 	if (lower_ > upper_)
 	{
-		
+		//fail
 	}
-
 }
 
-void RangeInt::setLower(int d0)
+void RangeInt::setLower(const int d0)
 {
-
-
 	lower_ = d0;
+	if (lower_ > upper_)
+	{
+		//fail
+	}
 }
 
-void RangeInt::setUpper(int d0)
+void RangeInt::setUpper(const int d0)
 {
-	if (d0 > )
-	{
-		
-	}
 	upper_ = d0;
+	if (lower_ > upper_)
+	{
+		//fail
+	}
+}
+
+void RangeInt::setValue(const int d0)
+{
+	if (limits_ && d0 < lower_ || d0 >= upper_)
+	{
+		//fail
+	}
+	value_ = d0;
 }
 
 const int RangeInt::getLower()
@@ -34,4 +44,9 @@ const int RangeInt::getLower()
 const int RangeInt::getUpper()
 {
 	return upper_;
+}
+
+const int RangeInt::getValue()
+{
+	return value_;
 }
--- CST136SRS01/RangeInt.h
+++ CST136SRS01/RangeInt.h
@@ -5,14 +5,18 @@ class RangeInt
 private:
 	int upper_{ 10 };
 	int lower_{ 0 };
-	int value_{0};
+	int value_{ 0 };
+	bool limits_{ false };
 
 public:
-	RangeInt(int d0, int d1);
+	RangeInt(const int d0, const int d1);
 	RangeInt();
 
-	void setUpper(int d0);
-	void setLower(int d0);
-	const int getUpper();
+	void setLower(const int d0);
+	void setUpper(const int d0);
+	void setValue(const int d0);
+
 	const int getLower();
+	const int getUpper();
+	const int getValue();
 };
