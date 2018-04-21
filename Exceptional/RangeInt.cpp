ff687d93e4538d903de616fb19fcb9c4d9ab338b
generalwax2018 <generalwax2018@users.noreply.github.com>
2018-04-21 00:01:37 -0700

    yeah yeah

--- Exceptional/RangeInt.cpp
+++ Exceptional/RangeInt.cpp
@@ -1,141 +1,226 @@
 #include "stdafx.h"
 #include "RangeInt.h"
+#include <algorithm>
+#include <sstream>
+#include <gsl.h>
 
 
-RangeInt::RangeInt()
-{
 
-}
 
 
 RangeInt::RangeInt(int const min, int const max)
 {
+	if (min >= max)
+	{
+		throw std::out_of_range("Lower Bound was greater than upper bound");
+	}
+	lower_bound_ = min;
+	upper_bound_ = max - 1;
 }
 
 int RangeInt::get_lower() const noexcept
 {
+	return lower_bound_;
 }
 
 int RangeInt::get_upper() const noexcept
 {
+	return upper_bound_;
 }
 
 int RangeInt::get_value() const noexcept
 {
+	return value_;
 }
 
 void RangeInt::set_value(int const num)
 {
+
+	set_value(gsl::narrow<int>(num));
+
 }
 
 void RangeInt::set_value(double const num)
 {
+	set_value(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_value(long const num)
 {
+	set_value(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_value(unsigned const num)
 {
+	set_value(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_value(string const num)
 {
+	istringstream iss(value_);
+
+	int temp;
+
+	iss >> temp;
+
+	set_value(temp); 
 }
 
-void RangeInt::set_value(long long const num)
-{
-}
 
 void RangeInt::set_value(float const num)
 {
+	set_value(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_value(char const num)
 {
+	istringstream iss(value_);
+
+	int temp;
+
+	iss >> temp;
+
+	set_value(temp); 
 }
 
 void RangeInt::set_value(char* const num)
 {
+	auto int_num{ 0 };
+	std::istringstream iss(num);
+	if (!(iss >> int_num || iss.bad()))
+	{
+		throw std::invalid_argument("String was bad");
+	}
+	set_value(int_num);
 }
 
 void RangeInt::set_value(short const num)
 {
+	set_value(gsl::narrow<int>(num));
 }
+
+
 
 void RangeInt::set_lower(int const num)
 {
+	if (num > value_ || num >= upper_bound_)
+	{
+		throw std::out_of_range("Tried to set lower bound out of range");
+	}
+	lower_bound_ = num;
 }
 
 void RangeInt::set_lower(double const num)
 {
+	set_lower(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_lower(long const num)
 {
+	set_lower(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_lower(unsigned const num)
 {
+	set_lower(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_lower(string const num)
 {
+	istringstream iss(value_);
+
+	int temp;
+
+	iss >> temp;
+
+	set_value(temp); 
 }
 
-void RangeInt::set_lower(long long const num)
-{
-}
 
 void RangeInt::set_lower(float const num)
 {
+	set_lower(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_lower(char const num)
 {
+	istringstream iss(value_);
+
+	int temp;
+
+	iss >> temp;
+
+	set_value(temp); 
 }
 
 void RangeInt::set_lower(char* const num)
 {
+	auto int_num{ 0 };
+	std::istringstream iss(num);
+	if (!(iss >> int_num || iss.bad()))
+	{
+		throw std::invalid_argument("String was bad");
+	};
+	set_lower(int_num);
 }
 
 void RangeInt::set_upper(int const num)
 {
+	set_upper(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_upper(double const num)
 {
+	set_upper(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_upper(long const num)
 {
+	set_upper(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_upper(unsigned const num)
 {
+	set_upper(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_upper(string const num)
 {
+	istringstream iss(value_);
+
+	int temp;
+
+	iss >> temp;
+
+	set_value(temp); 
 }
 
-void RangeInt::set_upper(long long const num)
-{
-}
 
 void RangeInt::set_upper(float const num)
 {
+	set_upper(gsl::narrow<int>(num));
 }
 
 void RangeInt::set_upper(char const num)
 {
+	istringstream iss(value_);
+
+	int temp;
+
+	iss >> temp;
+
+	set_value(temp); 
 }
 
 void RangeInt::set_upper(char* const num)
 {
+	auto int_num{ 0 };
+	std::istringstream iss(num);
+	if (!(iss >> int_num || iss.bad()))
+	{
+		throw std::invalid_argument("String was bad");
+	}
+	set_upper(int_num);
+
 }
 
-RangeInt::~RangeInt()
-{
-}
--- Exceptional/RangeInt.h
+++ Exceptional/RangeInt.h
@@ -19,7 +19,6 @@ public:
 	void set_value(long const num); 
 	void set_value(unsigned const num); 
 	void set_value(string const num); 
-	void set_value(long long const num); 
 	void set_value(float const num); 
 	void set_value(char const num); 
 	void set_value(char* const num); 
@@ -29,8 +28,7 @@ public:
 	void set_lower(double const num);
 	void set_lower(long const num); 
 	void set_lower(unsigned const num); 
-	void set_lower(string const num); 
-	void set_lower(long long const num); 
+	void set_lower(string const num);  
 	void set_lower(float const num); 
 	void set_lower(char const num); 
 	void set_lower(char* const num);
@@ -41,7 +39,6 @@ public:
 	void set_upper(long const num); 
 	void set_upper(unsigned const num); 
 	void set_upper(string const num); 
-	void set_upper(long long const num); 
 	void set_upper(float const num); 
 	void set_upper(char const num); 
 	void set_upper(char* const num); 
@@ -56,9 +53,6 @@ private:
 	static constexpr int Kunboundupper {numeric_limits<int>::max() -1};
 	static constexpr int Kunboundlower {numeric_limits<int>::min()};
 
-	
 
-	
-	~RangeInt();
 };
 
